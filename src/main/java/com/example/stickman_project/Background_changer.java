4cf527575c5dd8b6db1174426aa6d2bfb3aa11df
noeltiju <noelab04@gmail.com>
2023-12-09 20:58:27 +0530

    Bg changer

--- src/main/java/com/example/stickman_project/Background_changer.java
+++ src/main/java/com/example/stickman_project/Background_changer.java
@@ -1,5 +1,7 @@
 package com.example.stickman_project;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
@@ -20,30 +22,62 @@ public class Background_changer implements Runnable{
                 BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT)));
     }
-
-
-
-
-    @Override
-    public void run(){
-        while(true){
-            if (this.score_tracker.getScore() >= 2 && this.score_tracker.getScore() < 4){
-
+    int file_no = 1;
+    private Timeline changing_timeline = new Timeline(new KeyFrame(Duration.seconds(5), event ->{
+        switch (file_no) {
+            case 1:
+                this.main_pane.setBackground(new Background(new BackgroundImage(
+                        (new Image("bg.png")),
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundPosition.DEFAULT,
+                        BackgroundSize.DEFAULT)));
+                this.file_no = 2;
+                break;
+            case 2:
+                this.main_pane.setBackground(new Background(new BackgroundImage(
+                        (new Image("bg2.png")),
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundPosition.DEFAULT,
+                        BackgroundSize.DEFAULT)));
+                this.file_no = 3;
+                break;
+            case 3:
+                this.main_pane.setBackground(new Background(new BackgroundImage(
+                        (new Image("bg3.png")),
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundPosition.DEFAULT,
+                        BackgroundSize.DEFAULT)));
+                this.file_no = 4;
+                break;
+            case 4:
                 this.main_pane.setBackground(new Background(new BackgroundImage(
                         (new Image("bg4.png")),
                         BackgroundRepeat.NO_REPEAT,
                         BackgroundRepeat.NO_REPEAT,
                         BackgroundPosition.DEFAULT,
                         BackgroundSize.DEFAULT)));
-            }else if (this.score_tracker.getScore() >= 4 && this.score_tracker.getScore() < 8){
+                this.file_no = 5;
+                break;
+            case 5:
                 this.main_pane.setBackground(new Background(new BackgroundImage(
-                        (new Image("bg7.jpg")),
+                        (new Image("bg5.png")),
                         BackgroundRepeat.NO_REPEAT,
                         BackgroundRepeat.NO_REPEAT,
                         BackgroundPosition.DEFAULT,
                         BackgroundSize.DEFAULT)));
-            }
+                this.file_no = 1;
+                break;
         }
+
+    }));
+
+    @Override
+    public void run(){
+        this.changing_timeline.setCycleCount(Timeline.INDEFINITE);
+        changing_timeline.play();
 
     }
 
